d9be720dea80dd53b1404ce9ecb1c696206012d1
Quan Pham <doraemonfanclub@gmail.com>
2019-09-02 01:33:35 +0700

    Fixed Provider package version

---
+++ lib/providers/post_live_model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:sonix/api/api_post.dart';
+import 'package:sonix/models/post.dart';
+
+class PostProvider with ChangeNotifier {
+  List<Post> _posts;
+
+  getPosts() => _posts;
+
+  setPosts(List<Post> posts) => _posts = posts;
+
+  /// Get the posts from repository.
+  Future<List<Post>> fetchPosts ({page = 0, perPage = 30}) async {
+    var results = await ApiPost.getRecentPosts(page, perPage);
+    if (results.length > 0) {
+      _posts.addAll(results);
+      notifyListeners();
+    }
+    return results;
+  }
+}
